cf8e1ad8617e24d1197f7f3ff8fba5cd769c3364
PORTATIL-ADGS\ADGS <adgsgutierrez@gmail.com>
2017-05-11 18:20:33 -0500

    Ajuste a la bodega y al services

--- mavenproject1/nf_final/src/main/java/co/com/konrad/nt/ApplicationConfig.java
+++ mavenproject1/nf_final/src/main/java/co/com/konrad/nt/ApplicationConfig.java
@@ -31,8 +31,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(co.com.konrad.nt.ws.Imagenes.class);
-        resources.add(co.com.konrad.nt.ws.Usuario.class);
     }
     
 }
