12a429283b53cb91a16f9b19535cd56e8e24ecde
CesarR <CesarR@CESARR.DigitalWare.com.co>
2017-06-02 14:02:58 -0500

    Se genera el consumo de Tarjetas.

--- mavenproject1/nf_final/src/main/webapp/lib/js/perfil.js
+++ mavenproject1/nf_final/src/main/webapp/lib/js/perfil.js
@@ -14,8 +14,57 @@ $(document).ready(function () {
 function guardar () {
     usuario.usuarioNombre = $('#name').val();
     usuario.usuarioCorreo = $('#mail').val();
+    verificarPass();
 }
 
 function agregar () {
-    
+    $('#cards').show();
+    $('#Agregar').show();
+}
+
+function verificarPass() {
+   let pass1 = $('#passN').val();
+   let pass2 = $('#passVerify').val();
+   if (pass1 === pass2 && pass1 !== '' && pass2 !== '') {
+       usuario.usuarioClave = pass1;
+   Else
+       alert('Contraseña nueva Incorrecta');
+   }
+}
+
+function registrarTarjeta() {
+    let numero = $('#numero').val();
+    let fecha = $('#fvencimiento').val();
+    let cvc = $('#cvc').val();
+    let card = {
+        'tarjetaNumero': numero,
+        'tarjetaCvc': cvc,
+        'tarjetaFecha': fecha
+    };
+    if(card.tarjetaNumero === '' || card.tarjetaCvc === '' || card.tarjetaFecha === ''){
+        $("#error").html("Debes completar todos los campos para poder continuar.");
+    }else{
+        console.log(card);
+        $.ajax({
+            url: "services/usuario/Tarjetas",
+            data: JSON.stringify(card),
+            contentType:'application/json ; charset=utf-8',
+            dataType : 'json',
+            type: 'POST',
+            beforeSend: function () {
+
+            },
+            success: function (response) {
+                console.log(response);
+                if(response.codigo === 101){
+                    localStorage.setItem("tarjeta" , JSON.stringify(response.tarjeta));
+                    window.location.href = "perfil.html";
+                }else{
+                    $("#error").html(response.mensaje);
+                }
+            }, error: function (response) {
+                console.log(response);
+            }
+        });
+    }
 }
